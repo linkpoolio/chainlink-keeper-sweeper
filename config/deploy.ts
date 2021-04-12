387795a15dc7c3060050004c11a77c4ddabdd7e0
christianagnew <christian.agnew@outlook.com>
2021-04-12 17:03:55 -0400

    script to estimate gas

--- config/deploy.ts
+++ config/deploy.ts
@@ -2,7 +2,7 @@ export const config = {
   Keep3rSweeper: {
     rewardsWallet: '0x240BaE5A27233Fd3aC5440B5a598467725F7D1cd',
     minRewardsForPayment: 1000,
-    batchSize: 20,
+    batchSize: 30,
   },
   FluxAggregatorSweeper: {
     minToWithdraw: 100,
---
+++ scripts/estimate-gas.ts
@@ -0,0 +1,93 @@
+import { deployments, ethers } from 'hardhat'
+import { Contract } from 'ethers'
+import { config } from '../config/deploy'
+
+let keep3rSweeper: Contract
+let linkToken: Contract
+let fluxAggregatorSweeper: Contract
+let contracts: string[]
+
+async function setup() {
+  const node = config.FluxAggregatorSweeper.oracle
+
+  await deployments.fixture()
+
+  linkToken = await ethers.getContract('ERC677')
+  keep3rSweeper = await ethers.getContract('Keep3rSweeper')
+  const oracleSweeper = await ethers.getContract('OracleSweeper')
+  fluxAggregatorSweeper = await ethers.getContract('FluxAggregatorSweeper')
+  const offchainAggregatorSweeper = await ethers.getContract('OffchainAggregatorSweeper')
+
+  const Oracle = await ethers.getContractFactory('Oracle')
+  const FluxAggregator = await ethers.getContractFactory('FluxAggregator')
+  const OffchainAggregator = await ethers.getContractFactory('OffchainAggregator')
+
+  const oracles = []
+  const fluxAggregators = []
+  const offchainAggregators = []
+
+  for (let i = 0; i < 10; i++) {
+    let oracle = await Oracle.deploy(linkToken.address)
+    await oracle.transferOwnership(oracleSweeper.address)
+    await linkToken.transfer(oracle.address, ethers.utils.parseEther('100'))
+    oracles.push(oracle.address)
+
+    let fluxAggregator = await FluxAggregator.deploy(
+      linkToken.address,
+      [node],
+      [fluxAggregatorSweeper.address]
+    )
+    await linkToken.transfer(fluxAggregator.address, ethers.utils.parseEther('100'))
+    fluxAggregators.push(fluxAggregator.address)
+
+    let offchainAggregator = await OffchainAggregator.deploy(
+      linkToken.address,
+      [node],
+      [offchainAggregatorSweeper.address]
+    )
+    await linkToken.transfer(offchainAggregator.address, ethers.utils.parseEther('100'))
+    offchainAggregators.push(offchainAggregator.address)
+  }
+
+  contracts = oracles.concat(fluxAggregators).concat(offchainAggregators)
+
+  await oracleSweeper.addContracts(oracles)
+  await fluxAggregatorSweeper.addContracts(fluxAggregators)
+  await offchainAggregatorSweeper.addContracts(offchainAggregators)
+}
+
+async function main() {
+  await setup()
+  console.log('**** GAS ESTIMATES ****\n')
+
+  let checkUpkeep = await keep3rSweeper.checkUpkeep('0x00')
+  let toWithdraw = ethers.utils.defaultAbiCoder.decode(['uint256[][]'], checkUpkeep[1])[0]
+
+  console.log(
+    'withdraw rewards (30 contracts) -> ',
+    (await keep3rSweeper.estimateGas.withdraw(toWithdraw)).toNumber().toLocaleString()
+  )
+
+  await keep3rSweeper.withdraw(toWithdraw)
+  await linkToken.transfer(contracts[0], ethers.utils.parseEther('100'))
+
+  console.log(
+    'withdraw rewards (1 contract) -> ',
+    (await keep3rSweeper.estimateGas.withdraw([[0], [], []])).toNumber().toLocaleString()
+  )
+
+  console.log(
+    'add contracts to sweeper (30 contracts) -> ',
+    (await fluxAggregatorSweeper.estimateGas.addContracts(contracts.slice(0, 30)))
+      .toNumber()
+      .toLocaleString(),
+    '\n'
+  )
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
