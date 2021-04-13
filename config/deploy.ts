1d1f3688ba822a55958595d12321847600f9c3ca
christianagnew <christian.agnew@outlook.com>
2021-04-13 15:23:16 -0400

    adjust withdrawal minimums

--- config/deploy.ts
+++ config/deploy.ts
@@ -1,18 +1,18 @@
 export const config = {
   Keep3rSweeper: {
     rewardsWallet: '0x240BaE5A27233Fd3aC5440B5a598467725F7D1cd',
-    minRewardsForPayment: 1000,
+    minRewardsForPayment: 100,
     batchSize: 30,
   },
   FluxAggregatorSweeper: {
-    minToWithdraw: 100,
+    minToWithdraw: 20,
     oracle: '0x240BaE5A27233Fd3aC5440B5a598467725F7D1cd',
   },
   OffchainAggregatorSweeper: {
-    minToWithdraw: 100,
+    minToWithdraw: 20,
     transmitter: '0x240BaE5A27233Fd3aC5440B5a598467725F7D1cd',
   },
   OracleSweeper: {
-    minToWithdraw: 100,
+    minToWithdraw: 20,
   },
 }
