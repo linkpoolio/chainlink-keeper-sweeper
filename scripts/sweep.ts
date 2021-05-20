795d4f64bf6fa10ae8ccd1b0566eadfd00f6022c
christianagnew <christian.agnew@outlook.com>
2021-05-20 17:09:52 -0400

    use rapid gas price when sweeping

--- scripts/sweep.ts
+++ scripts/sweep.ts
@@ -1,4 +1,5 @@
 import { ethers } from 'hardhat'
+import { ethGas } from '../api/ethGas'
 
 async function main() {
   console.log('**** Sweeping rewards ****\n')
@@ -13,9 +14,16 @@ async function main() {
 
   while (checkUpkeep[0]) {
     let toWithdraw = ethers.utils.defaultAbiCoder.decode(['uint256[][]'], checkUpkeep[1])[0]
+
+    let gasPrice = await ethGas.get('')
     let gas = (await keeperSweeper.estimateGas.withdraw(toWithdraw)).toNumber()
-    let tx = await keeperSweeper.withdraw(toWithdraw, { gasLimit: gas + 100000 })
+
+    let tx = await keeperSweeper.withdraw(toWithdraw, {
+      gasPrice: gasPrice.data.data.rapid + 10000000000,
+      gasLimit: gas + 100000,
+    })
     await tx.wait()
+
     checkUpkeep = await keeperSweeper.checkUpkeep('0x00')
   }
 
