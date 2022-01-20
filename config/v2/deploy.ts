1de06212eb86a2edce3e6c6bdc7f308ed263a0df
christianagnew <christian.agnew@outlook.com>
2022-01-20 16:23:41 -0500

    v2 deployment script

---
+++ config/v2/deploy.ts
@@ -0,0 +1,10 @@
+export const config = {
+  OCRSweeper: {
+    rewardsPool: '', // address
+    ownerWallet: '', // address
+    profitMarginFeed: '', // address
+    rewardsPoolProfitShare: 0, // 2500 = 25%
+    minRewardForDistribution: 200, // 1 = 1 LINK
+    transmitter: '', // address
+  },
+}
---
+++ deploy/v2/1-deploy-ocr-sweeper.ts
@@ -0,0 +1,27 @@
+import { ethers } from 'hardhat'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { config } from '../config/v2/deploy'
+
+module.exports = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre
+  const { deploy } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  let linkToken: any = await ethers.getContractOrNull('LinkToken')
+  if (!linkToken) {
+    await deploy('LinkToken', {
+      from: deployer,
+      log: true,
+      args: ['Chainlink', 'LINK', 1000000000],
+    })
+    linkToken = await ethers.getContract('LinkToken')
+  }
+
+  await deploy('OCRSweeper', {
+    from: deployer,
+    log: true,
+    args: [linkToken.address, ...Object.values(config.OCRSweeper)],
+  })
+}
+
+module.exports.tags = ['V2-OCRSweeper']
